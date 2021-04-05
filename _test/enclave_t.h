c2b84e1ce1f8d720bb6a7b6800091ce759b46532
Ondřej Charbulák <xcharbul@fi.muni.cz>
2021-04-06 00:22:55 +0200

    Reworked cmake structure

---
+++ _test/enclave_t.h
@@ -0,0 +1,26 @@
+#ifndef ENCLAVE_T_H__
+#define ENCLAVE_T_H__
+
+#include <stdint.h>
+#include <wchar.h>
+#include <stddef.h>
+#include "sgx_edger8r.h" /* for sgx_ocall etc. */
+
+
+#include <stdlib.h> /* for size_t */
+
+#define SGX_CAST(type, item) ((type)(item))
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int test(int test_input_);
+
+sgx_status_t SGX_CDECL print_int(int i_);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-#include "enclave_wrapper.hpp"
 #include "sgx_urts.h"
+#include "untrusted/enclave_wrapper.hpp"
 
 #include <iomanip>
 #include <iostream>
@@ -9,8 +9,8 @@ int main()
     sgx_enclave_id_t eid     = 0;
     sgx_launch_token_t token = {0};
     int updated{};
-    std::cout << "Trying to create enclave from '" << ENCLAVE_FILE << "'" << std::endl;
-    const auto enclave_status = sgx_create_enclave(ENCLAVE_FILE, SGX_DEBUG_FLAG, &token, &updated, &eid, NULL);
+    std::cout << "Trying to create enclave from '" << SIGNED_LIB << "'" << std::endl;
+    const auto enclave_status = sgx_create_enclave(SIGNED_LIB, SGX_DEBUG_FLAG, &token, &updated, &eid, NULL);
 
     if (enclave_status != SGX_SUCCESS)
     {
