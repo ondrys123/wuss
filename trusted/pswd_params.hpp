7cf4d9133c8b8e0539c9f4a7bfca2f920778cf30
mdugacek <59182510+mdugacek@users.noreply.github.com>
2021-04-29 23:17:41 +0200

    Update trusted/pswd_params.hpp
    Co-authored-by: ondrys123 <31095922+ondrys123@users.noreply.github.com>

--- trusted/pswd_params.hpp
+++ trusted/pswd_params.hpp
@@ -5,7 +5,7 @@ namespace wuss
 {
 struct pswd_params_t
 {
-    uint32_t alpha_count; ///< slphabet characters count
+    uint32_t alpha_count; ///< alphabet characters count
     uint32_t num_count; ///< number count
     uint32_t symbol_count; ///< special symbol count
 };
